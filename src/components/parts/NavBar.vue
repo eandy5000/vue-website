aa6d893032d55b35920700b80333b483da2bc7a2
Andy Eccleston <eandy5000@yahoo.com>
2017-06-16 16:18:18 -0500

    added navbar component and set it up for desktop view

--- src/components/parts/NavBar.vue
+++ src/components/parts/NavBar.vue
@@ -1,15 +1,37 @@
 <template>
+    <div>
+    <p class="nav-title">Website Name</p>    
     <ul>
-        <li><router-link v-bind:to="{path: '/'}">Home</router-link></li>
-        <li><router-link v-bind:to="{path: '/about'}">About</router-link></li>
-        <li><router-link v-bind:to="{path: '/gallery'}">Gallery</router-link></li>
+        <li><router-link class="link" v-bind:to="{path: '/'}">Home</router-link></li>
+        <li><router-link class="link" v-bind:to="{path: '/about'}">About</router-link></li>
+        <li><router-link class="link" v-bind:to="{path: '/gallery'}">Gallery</router-link></li>
     </ul>
+    </div>
 </template>
 
 <script>
     export default {}
 </script>
 
-<style>
-    
+<style scoped>
+    div,
+    p {
+        display: flex;
+        margin: 0;
+        padding: 0;
+    }
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+    }
+    .link {
+        color: #2c3e50;
+        text-decoration: none;
+        margin-left: 1em;
+    }
+    .nav-title {
+        margin-right: auto;
+    }
 </style>
