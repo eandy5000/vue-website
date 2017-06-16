8ab03790f95b2fb12effcd769e84bce3b7626c80
Andy Eccleston <eandy5000@yahoo.com>
2017-06-16 16:29:21 -0500

    added basic gallery

--- src/components/pages/Gallery.vue
+++ src/components/pages/Gallery.vue
@@ -1,10 +1,35 @@
 <template>
-    <div>Gallery</div>
+    <div>Gallery
+    <carousel-3d>
+        <slide :index="0">
+
+        </slide>
+        <slide :index="1">
+        
+        </slide>
+        <slide :index="2">
+        
+        </slide>
+        <slide :index="3">
+        
+        </slide>
+        <slide :index="4">
+        
+        </slide>
+    </carousel-3d>
+    
+    </div>
 </template>
 
 <script>
+    import {Carousel3d, Slide} from 'vue-carousel-3d'
+
     export default {
-      name: 'Gallery'
+      name: 'Gallery',
+      components: {
+        Carousel3d,
+        Slide
+      }
     }
 </script>
 
