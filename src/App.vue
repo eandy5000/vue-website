8c9312aa47b1032b2a901363c56a2ae43b8ba984
Andy Eccleston <eandy5000@yahoo.com>
2017-08-05 08:23:58 -0500

    production version of donation button

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@
     <router-view class="view"></router-view>
     <div class="buttons">
 
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<form id="donate-button" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="J8VSDT9ZF666Q">
 
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<!--
+<input type="submit" value="Donate" >
+
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
--->
+
 </form>
 
 
@@ -71,7 +71,23 @@ $gray: #f3f2f2;
 }
 .buttons {
   position: fixed;
+    top: 80%;
+    right: -5%;
+  -webkit-transition: all .3s linear;
+  -moz-transition: all .3s linear;
+  transition: all .3s linear;
+}
+.buttons:hover,
+.buttons:active {
   top: 80%;
-  right: 1%;
+  right: -3%;
+}
+
+input[type=submit] {
+  background-color: $navy;
+  color: $white;
+  border: none;
+  font-weight: bold;
+  padding: .3em 5em .3em .7em;
 }
 </style>
