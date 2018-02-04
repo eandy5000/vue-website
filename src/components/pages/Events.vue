b2a8772a57fd3714e6ac50325653a54a123d1f1b
Andy Eccleston <eandy5000@yahoo.com>
2018-02-04 14:03:32 -0600

    pre events branch split

--- src/components/pages/Events.vue
+++ src/components/pages/Events.vue
@@ -2,7 +2,7 @@
     <div>
     <section>
     <h1 class="lead">Events</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam similique expedita nihil doloremque, et soluta ex quam hic alias odit maiores illum eius fugiat doloribus suscipit, omnis, labore quos pariatur.</p>
+    <p><span> {{foo}} </span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam similique expedita nihil doloremque, et soluta ex quam hic alias odit maiores illum eius fugiat doloribus suscipit, omnis, labore quos pariatur.</p>
     <action-call></action-call>
     </section>
     <section>
@@ -13,35 +13,42 @@
 </template>
 
 <script>
-    import ActionCall from '../parts/ActionCall.vue'
+import ActionCall from "../parts/ActionCall.vue";
+import foo from "../../assets/data/events-data.js";
 
-    export default {
-      name: 'events',
-      components: {
-        ActionCall
-      }
-    }
+export default {
+  name: "events",
+  components: {
+    ActionCall
+  },
+  data() {
+    return {
+      test1: "Hi there",
+      foo: foo.test
+    };
+  }
+};
 </script>
 
 <style scoped>
-  $navy: #003366;
-  $type: #2c3e50;
-  $red: #ff2400;
-  $white: #fffafa;
-  $gray: #cdc9c9;
+$navy: #003366;
+$type: #2c3e50;
+$red: #ff2400;
+$white: #fffafa;
+$gray: #cdc9c9;
 
-  div {
-      background-color: $white;
-  }
+div {
+  background-color: $white;
+}
 
-  section {
-    border: .1em solid $type;
-    border-radius: .3em;
-    background-color: $gray;
-    margin: 1em;
-  }
+section {
+  border: 0.1em solid $type;
+  border-radius: 0.3em;
+  background-color: $gray;
+  margin: 1em;
+}
 
-  .lead {
-    color: $navy;
-  }
+.lead {
+  color: $navy;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,31 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/components/pages/Home'
-import Meet from '../components/pages/Meet.vue'
-import NewGallery from '../components/pages/NewGallery.vue'
-import Events from '../components/pages/Events.vue'
-import Volunteer from '../components/pages/Volunteer.vue'
-import LogoHome from '../components/pages/LogoHome.vue'
-import BioPage from '../components/pages/Bio.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/pages/Home";
+import Meet from "../components/pages/Meet.vue";
+import NewGallery from "../components/pages/NewGallery.vue";
+import Events from "../components/pages/Events.vue";
+import Volunteer from "../components/pages/Volunteer.vue";
+import LogoHome from "../components/pages/LogoHome.vue";
+import BioPage from "../components/pages/Bio.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'LogoHome',
+      path: "/",
+      name: "LogoHome",
       component: LogoHome
     },
     {
-      path: '/bio',
-      name: 'Bio',
+      path: "/bio",
+      name: "Bio",
       component: BioPage
     },
     ,
     {
-      path: '/gal',
-      name: 'Gallery',
+      path: "/gal",
+      name: "Gallery",
       component: NewGallery
     }
     // {
@@ -34,14 +34,14 @@ export default new Router({
     //   component: Volunteer
     // },
     // {
-    //   path: '/events',
-    //   name: 'Events',
+    //   path: "/events",
+    //   name: "Events",
     //   component: Events
-    // },
+    // }
     // {
     //   path: '/test',
     //   name: 'NewHome',
     //   component: NewHome
     // }
   ]
-})
+});
