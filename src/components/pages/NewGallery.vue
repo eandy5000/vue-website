e347dd50845d0d192a74de8de958fb519d430441
Andy Eccleston <eandy5000@yahoo.com>
2018-02-11 17:46:04 -0600

    added more gallery images

--- src/components/pages/NewGallery.vue
+++ src/components/pages/NewGallery.vue
@@ -12,30 +12,65 @@
       <b-carousel-slide>
         <img 
           class="img-slide" 
-          :src="flParade" 
+          :src="flParade"
+          alt="Sheriff Starry greeting the crowd at the Forest Lake Parade 2017" 
         />
       </b-carousel-slide>
 
       <b-carousel-slide>
         <img 
           class="img-slide" 
-          :src="flParade2" 
+          :src="flParade2"
+          alt="Sheriff Starry greeting the crowd at the Forest Lake Parade 2017" 
         />
       </b-carousel-slide>
 
       <b-carousel-slide>
         <img 
           class="img-slide" 
-          :src="parade" 
+          :src="parade"
+          alt="Sheriff Starry greeting the crowd at the Cottage Grove Parade 2017" 
         />
       </b-carousel-slide>
 
       <b-carousel-slide>
         <img 
           class="img-slide" 
-          :src="cgParade" 
+          :src="cgParade"
+          alt="Sheriff Starry speaking with citizens at the Cottage Grove Parade 2017" 
         />
-        
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+        <img 
+          class="img-slide" 
+          :src="dan_and_smokey"
+          alt="Sheriff Starry with his K-9 faithful companion, Smokey, was with him from 2000-2005." 
+        />
+      </b-carousel-slide>
+
+       <b-carousel-slide>
+        <img 
+          class="img-slide" 
+          :src="st_jude"
+          alt="Sheriff Starry instructing D.A.R.E." 
+        />
+      </b-carousel-slide>
+
+       <b-carousel-slide>
+        <img 
+          class="img-slide" 
+          :src="dan_family"
+          alt="Sheriff Starry’s family- Dan, his wife Jeanice, daughters Bethany and Megan"  
+        />
+      </b-carousel-slide>
+
+       <b-carousel-slide>
+        <img 
+          class="img-slide" 
+          :src="dan_students_stillwater"
+          alt="Sheriff Starry speaking with children at a Stillwater School" 
+        />
       </b-carousel-slide>
 
 
@@ -46,35 +81,39 @@
 
 
 <script>
-    import flParade from '../../assets/fl_parade_1.jpg';
-    import flParade2 from '../../assets/fl_parade_2.jpg';
-    import cgParade from '../../assets/cg_parade.jpg';
-    import parade from '../../assets/dan-starry-for-sheriff-campaign-event.jpg';
+import flParade from "../../assets/fl_parade_1.jpg";
+import flParade2 from "../../assets/fl_parade_2.jpg";
+import cgParade from "../../assets/cg_parade.jpg";
+import parade from "../../assets/dan-starry-for-sheriff-campaign-event.jpg";
+import dan_and_smokey from "../../assets/Dan_and_Smokey.jpg";
+import st_jude from "../../assets/Dan_DARE_St_Judes.jpg";
+import dan_family from "../../assets/Dan_Family.jpg";
+import dan_students_stillwater from "../../assets/Dan_Students_Stillwater.jpg";
 
-    export default {
-      name: 'Gallery',
-      components: {
-
-      },
-      data () {
-        return {
-          flParade,
-          flParade2,
-          cgParade,
-          parade
-        }
-      }
-    }
+export default {
+  name: "Gallery",
+  components: {},
+  data() {
+    return {
+      flParade,
+      flParade2,
+      cgParade,
+      parade,
+      dan_and_smokey,
+      st_jude,
+      dan_family,
+      dan_students_stillwater
+    };
+  }
+};
 </script>
 
 <style>
-
-    .img-slide {
-      max-width: 75%;
-      margin: 1em auto;
-    }
-    .jumbotron {
-      margin-bottom: 0 !important;
-    }
-
+.img-slide {
+  max-width: 75%;
+  margin: 1em auto;
+}
+.jumbotron {
+  margin-bottom: 0 !important;
+}
 </style>
