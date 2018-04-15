57f697151321c345bee0f7a79da616719c63a7ab
Andy Eccleston <eandy5000@yahoo.com>
2018-04-15 16:53:39 -0500

    new event added

--- src/assets/data/events-data.js
+++ src/assets/data/events-data.js
@@ -2,6 +2,17 @@ module.exports = {
   textContact: "Text STARRY to 33222",
   events: [
     {
+      date: "April 19th, 2018",
+      location: "Tamarack Tap Room",
+      address: "8418 Tamarack Tap Room Woodbury MN 55125",
+      time: "5:00 PM to 7:00 PM",
+      description:
+        "You’re Invited to a Fundraiser to help support the Campaign to keep Dan Starry as the Washington County Sheriff",
+      type: "Fundraiser",
+      map:
+        "https://www.google.com/maps/dir/''/google+maps+8418+Tamarack+Tap+Room+Woodbury+MN+55125"
+    },
+    {
       date: "April 7th, 2018",
       location: "Stillwater Country Club",
       address: "1421 4th Street North Stillwater MN 55082",
